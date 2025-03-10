b7291358c91b08cc26f315c29f8a8aa8970e1499
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-10 14:50:09 -0300

    Masterizado

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,50 +4,71 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("Calculadora de Malandro");
-            Console.WriteLine("------------------------------");
-
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("3 - Sair");
-           
-            Console.Write("Escolha uma Opção: ");
-
-            string opcao = Console.ReadLine().ToUpper(); // Maiúsculo
-           
-
-            //Estrutura de decisão (se/senao)
-            if (opcao == "S")
+            //loop 0 - 1
+            while (true)
             {
-                return;
+                Console.Clear();
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Calculadora de Malandro");
+                Console.WriteLine("------------------------------");
+
+                Console.WriteLine(" 1 - Somar");
+                Console.WriteLine(" 2 - Subtrair");
+                Console.WriteLine(" 3 - Multiplicar");
+                Console.WriteLine(" 4 - Dividir");
+                Console.WriteLine(" 5 - Sair");
+
+                Console.Write("\n Escolha uma Opção: ");
+
+                string opcao = Console.ReadLine().ToUpper(); // Maiúsculo
+
+                //Estrutura de decisão (se/senao)
+                if (opcao == "5")
+                    break;
+
+
+
+                Console.Write(" Digite o Primeiro Número: ");
+                string primeiroNumeroString = Console.ReadLine();
+                decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString); 
+
+                Console.Write(" Digite o Primeiro Número: ");
+                string segundoNumeroString = Console.ReadLine();
+                decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
+
+                decimal resultado = 0;
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+
+                if (opcao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+                if (opcao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                }
+
+                else if (opcao == "4")
+                {
+                    resultado = primeiroNumero / segundoNumero;
+                }
+
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("\nResultado: " + resultado.ToString("F2")); // FloatingPoints = 2
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("\nDeseja Continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                {
+                    break;
+                }
             }
-
-            Console.Write("Digite o Primeiro Número: ");
-            string primeiroNumeroString = Console.ReadLine();
-            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
-
-            Console.Write("Digite o Primeiro Número: ");
-            string segundoNumeroString = Console.ReadLine();
-            int segundoNumero = Convert.ToInt32(segundoNumeroString);
-
-            int resultado = 0;
-
-            if (opcao == "1")
-            {
-                 resultado = primeiroNumero + segundoNumero;
-            }
-
-
-            else 
-            {
-                 resultado = primeiroNumero - segundoNumero;
-            }
-            
-            Console.WriteLine("\n" + resultado);
-
-            Console.ReadLine(); // leia
-
         }
     }
 }
